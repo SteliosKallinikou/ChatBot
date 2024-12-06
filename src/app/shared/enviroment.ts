37613da63d0977cd4c61f62f90d8d98e707f91dc
SteliosKallinikou <stelios.kallinikou@akelius.com>
2024-12-06 17:26:12 +0200

    Response and communication with bot

---
+++ src/app/shared/enviroment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  openaiApiKey: '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'
+}
